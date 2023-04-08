7580ac17a5b02435084e01307557cc07140c91b0
surathjhakal <jhakal.surath@gmail.com>
2023-04-08 13:31:13 +0530

    did configuration for deployment on railway.app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangoProjects.wsgi --log-file-
--- djangoProjects/settings.py
+++ djangoProjects/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
+DATABASE_URL="postgresql://postgres:mObq0oA8nwQ3BMSeUS4m@containers-us-west-4.railway.app:6763/railway"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +29,7 @@ SECRET_KEY = 'cisr^2wf6rafjmsy$cnn!vl62_%pifpvxj1vebp#6^rcwz^)ph'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,11 +78,26 @@ WSGI_APPLICATION = 'djangoProjects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# DATABASES={
+#    'default':{
+#       'ENGINE':'django.db.backends.postgresql_psycopg2',
+#       'NAME':'postgres',
+#       'USER':'postgres',
+#       'PASSWORD':'admin',
+#       'HOST':'localhost',
+#       'PORT':'5432',
+#    }
+# }
+
+DATABASES={
+    "default":dj_database_url.config(default=DATABASE_URL,conn_max_age=1800)
 }
 
 
@@ -120,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     STATICFILES_DIR,
 ]
